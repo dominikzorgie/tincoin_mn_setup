50bd3a96a5bbe30453d41827de7b188019fbf27e
Michael Steger <mjsteger1@gmail.com>
2018-02-03 21:46:14 -0800

    Remove unreliable tincoin-cli command
    And some other cleanup/bugfixes

--- tincoin_mn_setup.rb
+++ tincoin_mn_setup.rb
@@ -2,6 +2,8 @@
 
 require 'tty-spinner'
 
+LOG_FILE = '/tmp/tin_mn.log'
+
 print "Please enter your masternode key, which we'll need later: "
 mnkey = gets.chomp
 
@@ -9,26 +11,27 @@ spinner = TTY::Spinner.new("[:spinner] :title")
 spinner.update(title: "Starting install...")
 spinner.auto_spin
 
-def wait_for_process(text, find: nil)
+def wait_for_process(text, find: nil, nowait: false)
+  File.open(LOG_FILE, 'a+') { |f| f.write("Now going to run #{text}") }
   proc = IO.popen(text, err: [:child, :out])
-  Process.waitpid2(proc.pid)
+  Process.waitpid2(proc.pid) unless nowait
+  return proc.read =~ /#{find}/ unless find.nil?
 
-  return File.open('/tmp/tin_mn.log', 'a+') { |f| f.write(proc.read) }
-  return proc.read =~ /#{find}/ if !find.nil?
+  File.open(LOG_FILE, 'a+') { |f| f.write(proc.read) }
 end
 
 spinner.update(title: "Installing base dependencies...")
 
-wait_for_process("sudo apt-get update &&\
-sudo apt-get install -y git && \
-git clone https://github.com/tincoinpay/tincoin.git")
+wait_for_process("sudo apt-get update")
+wait_for_process("sudo apt-get install -y git")
+wait_for_process("git clone https://github.com/tincoinpay/tincoin.git")
+wait_for_process("sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev")
+wait_for_process("sudo add-apt-repository -yu ppa:bitcoin/bitcoin")
 
-wait_for_process("sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
- sudo add-apt-repository -y ppa:bitcoin/bitcoin &&  \
- sudo apt-get update && \
- sudo apt-get install -y libdb4.8-dev libdb4.8++-dev && \
-  sudo apt-get install -y libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler && \
-  sudo apt-get install -y libminiupnpc-dev libzmq3-dev libqrencode-dev")
+# For some reason waiting here stalls forever. Not sure what's up, but we don't
+# need to wait on the install here, as it'll take a bit to make swap/set up the
+# configuration before the make step starts
+wait_for_process("sudo apt-get install -y libdb4.8-dev libdb4.8++-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libminiupnpc-dev libzmq3-dev libqrencode-dev", nowait: true)
 
 spinner.update(title: "Making swap...")
 
@@ -38,14 +41,15 @@ spinner.update(title: "Building tincoin source, this could take a while...")
 
 wait_for_process("cd ~/tincoin &&  ./autogen.sh && ./configure && make && make install")
 
-spinner.update(title: "Awesome! That's been installed.")
+spinner.update(title: "Okay, tincoin has now been installed! Setting up config...")
 
 sleep(5)
 
 ip = %x(hostname -I|cut -f1 -d ' ').chomp
 
 # We do this to create all the config files
-wait_for_process("tincoind -daemon && tincoin-cli stop")
+wait_for_process("mkdir -p ~/.tincoincore")
+
 
 # Setup config
 File.open(Dir.home + "/.tincoincore/tincoin.conf", 'w') do |f|
@@ -68,19 +72,22 @@ spinner.update(title: "Please update your local wallet config. Your ip is #{ip} 
 
 # Loop on trying to start mn; continue on success
 
+wait_for_process("tincoind -daemon", nowait: true)
+
 until wait_for_process("tincoin-cli masternode start local false; tincoin-cli masternode status", find: "Masternode successfully started")
   sleep 10
 end
 
 spinner.update(title: "Setting up sentinel...")
 
-wait_for_process("cd ~/.tincoincore &&  /root/tincoin/src/tincoin-cli stop && rm mncache.dat && rm mnpayments.dat && /root/tincoin/src/tincoind -daemon -reindex")
+# TODO Think this isn't needed, remove if so
+# wait_for_process("cd ~/.tincoincore &&  /root/tincoin/src/tincoin-cli stop && rm mncache.dat && rm mnpayments.dat && /root/tincoin/src/tincoind -daemon -reindex")
 
-wait_for_process("sudo apt-get update && sudo apt-get -y install python python-virtualenv && cd ~/ && git clone https://github.com/tincoinpay/sentinel.git && cd sentinel")
+wait_for_process("sudo apt-get update && sudo apt-get -y install python python-virtualenv && cd ~/ && git clone https://github.com/tincoinpay/sentinel.git ~/sentinel && cd sentinel")
 
-wait_for_process("virtualenv ./venv && ./venv/bin/pip install -r requirements.txt")
+wait_for_process("cd ~/sentinel && virtualenv ./venv && ./venv/bin/pip install -r requirements.txt")
 
-wait_for_process("echo tincoin_conf=/root/.tincoincore/tincoin.conf >> sentinel.conf")
+wait_for_process("echo tincoin_conf=/root/.tincoincore/tincoin.conf >> ~/sentinel/sentinel.conf")
 
 spinner.update(title: "Waiting for sync to finish")
 
